c93bf12d808ba8619b6ef5b353f33bc0cd22b5c8
xuyizhou <xu.yizhou@outlook.com>
2022-10-09 22:35:34 +0800

    1742. 盒子中小球的最大数量

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <unordered_map>
+using namespace std;
+
+// 1742. 盒子中小球的最大数量
+int sumOfValue(int num)
+{
+    int remainder;
+    int sum = 0;
+    while (num>0)
+    {
+        sum += (num % 10);
+        num /= 10;
+    }
+    return sum;
+}
+int countBalls(int lowLimit, int highLimit)
+{
+    int box[100] = {0};
+    int max=0;
+    for (int i = lowLimit; i <= highLimit; i++)
+    {
+        int sum = sumOfValue(i);
+        box[sum]++;
+        if (box[sum]>max)
+        {
+            max=box[sum];
+        }
+    }
+    return max;
+}
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    int ret = countBalls(19,28);
+    cout<<ret<<endl;
 }
